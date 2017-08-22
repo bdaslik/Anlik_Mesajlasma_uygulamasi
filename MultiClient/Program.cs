ef34c20d50f042f6bff2a5795ec528457d393a9b
Bervan Daşlık <bervandaslik@gmail.com>
2017-08-22 18:35:19 +0300

    Final Sürümü
    Uygulama çalıştırılabilir.

---
+++ MultiClient/Program.cs
@@ -0,0 +1,375 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+
+
+namespace MultiClient
+{
+    class Program
+    {
+        private static readonly Socket ClientSocket = new Socket
+            (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static Thread thread1 = new Thread(new ThreadStart(receive));
+
+        private const int PORT = 100;
+        private const string serverIP = "192.168.1.101";
+
+        private static int pieceUser;
+        private static string RecUser,SendUser;
+
+        private static SqlConnection baglanti;
+        private static SqlCommand komut;
+        private static SqlDataReader reader;
+        
+
+        static void Main()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.SetWindowSize(70, 35);
+            Console.Clear();
+
+            Console.Title = "Mesajlaşma Istemcisi";
+            
+            ConnectToServer(serverIP);
+            baglanti = new SqlConnection();
+            //  baglanti.ConnectionString = "Data Source=192.168.1.101,1433;Network Library=DBMSSOCN;Initial Catalog=socketmesaj;User ID=bervan;Password=cod_8580;";
+            baglanti.ConnectionString = "Server="+serverIP+";Database=socketmesaj;User Id=bervan;Password = cod_8580; ";
+            komut = new SqlCommand();
+            komut.Connection = baglanti;
+
+            userlogin();
+            menu();
+
+           
+        }
+
+        private static void userlogin()
+        {
+            string kullanici, pass;
+
+            Console.Write("Kullanıcı Adınız :");
+            kullanici = Console.ReadLine();
+            Console.Write("Şifreniz :");
+            pass = Console.ReadLine();
+
+
+            komut.CommandText = "SELECT * FROM db_users WHERE kadi='" + kullanici + "'AND sifre='" + pass + "'";
+
+            baglanti.Open();
+            reader = komut.ExecuteReader();
+
+            if (reader.Read())
+            {
+                Console.WriteLine("Giriş Başarılı\nDevam etmek için bir tuşa basınız..");
+                Console.ReadKey();
+                Console.Clear();
+                SendUser = kullanici;
+                sendUserName(SendUser);
+                baglanti.Close();
+                offline_messages();
+            }
+
+            else
+            {
+                Console.WriteLine("Kullanıcı Adı veya Şifre yanlış!!!");
+                baglanti.Close();
+                userlogin();
+            }
+
+
+            
+
+
+        }
+
+        private static void sendUserName(string text)
+        {
+            text = "100" + ";" + " " + ";" + SendUser + ";" + " ";
+            byte[] buffer = Encoding.ASCII.GetBytes(text);
+            ClientSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+        }
+
+        private static void offline_messages()
+        {
+            SqlCommand guncelle= new SqlCommand();
+            guncelle.Connection = baglanti;
+            komut.CommandText = "SELECT * FROM messages WHERE alici='" + SendUser + "'AND iletim='" + 0 + "'";
+
+            baglanti.Open();
+            reader = komut.ExecuteReader();
+
+            Console.Clear();
+            Console.WriteLine("Okunmamış mesajlar \n");
+
+            while (reader.Read())
+            {
+                Console.WriteLine("{0} : {1}",reader[1],reader[3]);
+            }
+            reader.Close();
+            guncelle.CommandText = "UPDATE messages SET iletim='1' WHERE alici='"+ SendUser+"'";
+            guncelle.ExecuteNonQuery();
+            baglanti.Close();
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        private static void menu()
+        {
+            int secim;
+
+            Console.WriteLine("Anlık Mesajlaşma v1.0\nMenü\n");
+            Console.WriteLine("1-)Bireysel Mesajlaşma");
+            Console.WriteLine("2-)Grupla Mesajlaşma");
+            Console.WriteLine("3-)Herkese Mesaj At");
+
+            Console.Write("\nSeçiminiz :");
+            secim=Convert.ToInt32(Console.ReadLine());
+            
+            switch (secim)
+            {
+                case 1:
+                    singleMessage();
+                    break;
+                case 2:
+                    groupMessage();
+                    break;
+                case 3:
+                    sendEveryone();
+                    break;
+                default:
+                    Console.WriteLine("Hatalı Seçim!!");
+                    Console.ReadKey();
+                    Console.Clear();
+                    menu();
+                    break;
+            }
+        }
+        
+        private static void singleMessage()
+        {
+            string AliciAd;
+            Console.WriteLine("Alıcı adını giriniz :");
+            AliciAd = Console.ReadLine();
+
+            komut.CommandText = "SELECT * FROM db_users WHERE kadi='" + AliciAd + "'";
+
+            baglanti.Open();
+            reader = komut.ExecuteReader();
+
+            if (reader.Read())
+            {
+                Console.WriteLine("Bağlantı kuruldu..");
+                Console.ReadKey();
+                Console.Clear();
+            }
+            else
+            {
+                Console.WriteLine("Böyle bir kullanıcı bulunamadı.\n Kullanıcı adını tekrar giriniz..");
+                Console.ReadKey();Console.Clear();
+
+                baglanti.Close();
+                singleMessage();
+                return;
+            }
+
+
+            baglanti.Close();
+            
+            RecUser = AliciAd;
+            Console.Title = "Bireysel Sohbet - " + SendUser + " >> " + RecUser;
+            pieceUser = 1;
+            RequestLoop();
+            Exit();
+
+        }
+
+        private static void groupMessage()
+        {
+            int secim;
+            string group_name,kisiler;
+            Console.WriteLine("Grup Sohbeti\n1-)Yeni Grup Oluştur\n2-)Var olan gruba yaz.");
+            Console.Write("Seciminiz :");
+            secim = Convert.ToInt32(Console.ReadLine());
+
+            #region Grup Sohbeti Menüsü
+            if (secim==1)
+            {
+                Console.Write("Grup adı :");
+                group_name=Console.ReadLine();
+                Console.WriteLine("Gruba eklemek istediğiniz kişiler (aralarına virgül koyun)");
+                kisiler = Console.ReadLine();
+                kisiler += "," + SendUser;
+
+                komut.Connection = baglanti;
+                komut.CommandText = "INSERT INTO groups (grupAd,kisiler) VALUES ('" + group_name + "','" + kisiler + "')";
+                baglanti.Open();
+
+                int sonuc = komut.ExecuteNonQuery();
+                baglanti.Close();
+
+                if (sonuc > 0) Console.WriteLine("Grup Başarıyla oluşturuldu");
+                else { Console.WriteLine("Grup oluşturma başarısız"); Console.ReadKey(); return; }
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+            else if (secim==2)
+            {
+                while (true)
+                {
+                    Console.Write("Grup adı :");
+                    group_name = Console.ReadLine();
+
+                    komut.Connection = baglanti;
+                    komut.CommandText = "SELECT * FROM groups WHERE grupAd='" + group_name + "'";
+                    baglanti.Open();
+                    reader = komut.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        kisiler = reader[2].ToString() ;
+                        baglanti.Close();
+                        break;
+                    }
+                    Console.WriteLine("Grup Bulunamadı!!");
+                    reader.Close();
+                    baglanti.Close();
+                }
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Hatalı Seçim!!");
+                groupMessage();
+                return;
+            }
+            #endregion
+
+
+            Console.Clear();
+            string[] temp = kisiler.Split(',');
+            Console.Title = "Grup Sohbeti - " + SendUser + " >> " + group_name;
+            pieceUser = temp.Length;
+            RecUser = group_name;
+            RequestLoop();
+            Exit();
+
+        }
+
+        private static void sendEveryone()
+        {
+            pieceUser = 0; //herkese gönderilecegini belirtiyoruz
+
+            RecUser = "Herkes";
+            Console.Title = "Herkese Yayın Mesajı - " + SendUser + " >> Herkese" ;
+            RequestLoop();
+            Exit();
+        }
+
+        private static void ConnectToServer(string serverIP)
+        {
+            int attempts = 0;
+
+            while (!ClientSocket.Connected)
+            {
+                try
+                {
+                    attempts++;
+                    Console.WriteLine("Tekrar Bağlanıyor ({0})" , attempts);
+                    
+                    ClientSocket.Connect(IPAddress.Parse(serverIP), PORT);
+                }
+                catch (SocketException) 
+                {
+                    Console.Clear();
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine("Sunucuya Bağlandı.");
+        }
+
+        private static void RequestLoop()
+        {
+            Console.WriteLine(@"<Uygulamayı sonlandırmak için ""cikis"" yazınız.>");
+            Console.WriteLine(GetLocalIPAddress());
+            
+            thread1.Start();
+            while (true)
+            {
+                SendRequest();
+            }
+        }
+
+        private static void receive() {
+            var buffer = new byte[2048];
+            int received = ClientSocket.Receive(buffer, SocketFlags.None);
+            if (received == 0) return;
+            else
+            {
+                ReceiveResponse(buffer, received);
+            }
+            Thread.Sleep(500);
+            receive();
+        }
+        
+        private static void Exit()
+        {
+            
+            SendString("cikis"); 
+            ClientSocket.Shutdown(SocketShutdown.Both);
+            ClientSocket.Close();
+            Environment.Exit(0);
+        }
+
+        private static void SendRequest()
+        {
+            string request = Console.ReadLine();
+            SendString(request);
+            if (request.ToLower() == "cikis")
+            {
+                Exit();
+            }
+            
+        }
+   
+        private static void SendString(string text)
+        {
+            if(text!="cikis")
+                text = pieceUser.ToString() + ";" + RecUser + ";" + SendUser + ";" + text;
+
+            byte[] buffer = Encoding.ASCII.GetBytes(text);
+            ClientSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+        }
+
+        private static void ReceiveResponse(byte[] buffer,int received)
+        {
+            var data = new byte[received];
+            Array.Copy(buffer, data, received);
+            string text = Encoding.ASCII.GetString(data);
+            string[] token = text.Split(';');
+
+            Console.WriteLine("\n{0} :{1}",token[0],token[1]);
+            
+        }
+
+        static string GetLocalIPAddress()
+        {
+            string ip=null;
+            foreach (IPAddress adres in Dns.GetHostAddresses(Dns.GetHostName()))
+            {
+
+                ip = adres.ToString();
+
+            }
+            return ip;
+        }
+
+    }
+}
---
+++ MultiServer/Program.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MultiServer
+{
+    class Program
+    {
+
+        #region Statik Tanımlamalar
+        private static readonly Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        private static readonly List<Socket> clientSockets = new List<Socket>();
+        private static readonly List<string> users = new List<string>();
+
+        private const int BUFFER_SIZE = 2048;
+        private const int PORT = 100;
+        private static readonly byte[] buffer = new byte[BUFFER_SIZE];
+
+        private static SqlConnection baglanti;
+        private static SqlCommand komut;
+        private static SqlDataReader reader;
+        #endregion
+
+
+        static void Main()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.SetWindowSize(70, 35);
+            Console.Clear();
+
+            Console.Title = "Mesajlaşma Server";
+            SetupServer();
+
+            while (Console.ReadLine()!="cikis") ;
+
+            CloseAllSockets();
+            
+        }
+
+        private static void SetupServer()
+        {
+            Console.WriteLine("Server ayarları yapılıyor.");
+            serverSocket.Bind(new IPEndPoint(IPAddress.Any, PORT));
+            serverSocket.Listen(0);
+            serverSocket.BeginAccept(AcceptCallback, null);
+
+            baglanti = new SqlConnection();
+            baglanti.ConnectionString = "Data Source=.;Initial Catalog=socketmesaj;Integrated Security=SSPI";
+            komut = new SqlCommand();
+            komut.Connection = baglanti;
+
+            Console.WriteLine("Server hazır.");
+
+
+        }
+        
+        private static void CloseAllSockets()
+        {
+            foreach (Socket socket in clientSockets)
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+
+            serverSocket.Close();
+        }
+
+        private static void AcceptCallback(IAsyncResult AR)
+        {
+            Socket socket;
+
+            try
+            {
+                socket = serverSocket.EndAccept(AR);
+            }
+            catch (ObjectDisposedException) 
+            {
+                return;
+            }
+
+           clientSockets.Add(socket);
+           socket.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, socket);
+           Console.WriteLine("Istemci baglandi. Istek bekleniyor.");
+
+            
+
+           serverSocket.BeginAccept(AcceptCallback, null);
+        }
+
+        private static void ReceiveCallback(IAsyncResult AR)
+        {
+            #region Tanımlamalar
+            Socket current = (Socket)AR.AsyncState;
+            Console.WriteLine(current.RemoteEndPoint);
+            int received;
+            #endregion
+
+            #region Try-catch Bloğu
+
+            try
+            {
+                received = current.EndReceive(AR);
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Istemci Baglantisini Kesti.");
+
+                int index;
+                index = clientSockets.IndexOf(current);
+                if (users.Count>index) users.Remove(users[index]);
+                current.Close(); 
+                clientSockets.Remove(current);
+                
+                return;
+            }
+
+            #endregion
+
+            #region İstemciden gelen mesaj
+
+            byte[] recBuf = new byte[received];
+            Array.Copy(buffer, recBuf, received);
+            string text = Encoding.ASCII.GetString(recBuf);
+            Console.WriteLine("Gelen Mesaj: " + text);
+
+            #endregion
+
+            #region Sunucudan zaman isteginde bulunma
+
+            //if (text.ToLower() == "zaman") // sunucudan zaman talebi
+            //{
+            //    Console.WriteLine("Zaman istegi yapildi.");
+            //    byte[] data = Encoding.ASCII.GetBytes(DateTime.Now.ToLongTimeString());
+            //    current.Send(data);
+            //    Console.WriteLine("Zaman istemciye gönderildi");
+            //}
+            //else 
+
+            #endregion
+
+            #region istemcinin baglanti koparmasi
+
+            if (text.ToLower() == "cikis")
+            {
+                
+                current.Shutdown(SocketShutdown.Both);
+                current.Close();
+                int index;
+                index=clientSockets.IndexOf(current);
+                users.Remove(users[index]);
+                clientSockets.Remove(current);
+
+
+                Console.WriteLine("Istemci baglantisi koparıldı");
+                return;
+            }
+
+            #endregion
+
+            
+            else
+            {
+                
+                string[] token = text.Split(';');  //Gelen mesajı parçalama
+
+                #region kullanıcı servera kullanıcı adını göndermişse 
+
+                if (token[0]=="100")  
+                {
+                    users.Add(token[2]);
+                }
+                #endregion
+
+                #region Bireysel sohbet mesajı ise
+                else if (Convert.ToInt32(token[0])==1) 
+                {
+                    
+                    sendMessage(token[1],token[2] ,token[3]);
+
+                    Console.WriteLine("Sohbet Mesajı iletildi");
+                }
+                #endregion
+
+                #region Grup sohbeti ise
+                else if (Convert.ToInt32(token[0])>1)
+                {
+                    string kisiler;
+                    
+
+                    komut.Connection = baglanti;
+                    komut.CommandText = "SELECT * FROM groups WHERE grupAd='" + token[1] + "'";
+                    baglanti.Open();
+                    reader = komut.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        kisiler = reader[2].ToString();
+                        string[] grup = kisiler.Split(',');
+                        baglanti.Close();
+                        reader.Close();
+
+                        for (int i = 0; i < Convert.ToInt32(token[0]); i++)
+                        {
+                            if (grup[i] == token[2]) continue;
+                            sendMessage(grup[i],token[2],token[3]);
+
+                            Console.WriteLine("Sohbet Mesajı iletildi");
+                        }
+                        
+                    }
+                    
+                }
+
+                #endregion
+
+                #region Herkese (Yayın Mesajı ise)
+                else if (Convert.ToInt32(token[0])==0)
+                {
+                    List<string> alici = new List<string>();
+
+
+                    komut.Connection = baglanti;
+                    komut.CommandText = "SELECT kadi FROM db_users";
+                    baglanti.Open();
+                    reader = komut.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        alici.Add(reader[0].ToString());
+                    }
+
+                    baglanti.Close();
+                    reader.Close();
+
+                    for (int i = 0; i < alici.Count; i++)
+                    {
+                        if (alici[i] == token[2]) continue;
+                        sendMessage(alici[i], token[2], token[3]);
+
+                        Console.WriteLine("Sohbet Mesajı iletildi");
+                    }
+                }
+
+                #endregion
+
+                
+                
+            }
+
+            //İstek işlendikten sonra yeni istek bekleme
+            current.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, current);
+
+        }
+
+
+        private static void sendMessage(string receive,string sender,string message)
+        {
+            Socket client= null;
+           
+            if (users.IndexOf(receive) != -1)//Gönderilen istemci aktif ise mesajı gönderir
+            {
+
+                client = clientSockets[users.IndexOf(receive)];
+                byte[] data = Encoding.ASCII.GetBytes(sender + ";" + message);
+                client.Send(data);
+                komut.CommandText = "INSERT INTO messages (gonderen,alici,mesaj,iletim) VALUES ('" + sender + "','" + receive + "','" + message + "','1')";
+            }
+            else//Değilse veritabanına aktif degil girdisi girilir.
+            {
+                Console.WriteLine("Kullanıcı aktif degil.Online olunca mesaj iletilecektir.");
+                komut.CommandText = "INSERT INTO messages (gonderen,alici,mesaj,iletim) VALUES ('" + sender + "','" + receive + "','" + message + "','0')";
+            }
+
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+
+
+        }
+
+    }
+}
